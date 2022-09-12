9eba8f03ecbe859c751ba87ea6f0b3cd5d8218f8
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-09-12 15:47:18 -0500

    Validações de Login #02

--- src/config/loader.php
+++ src/config/loader.php
@@ -17,4 +17,20 @@ function loadView($ViewName, $params = array()) {
     require_once(VIEW_PATH . "/{$ViewName}.php");
 }
 
+function loadTemplateView($ViewName, $params = array()) {
+
+    if(count($params) > 0) {
+        foreach($params as $key => $value) {
+            if(strlen($key) > 0){
+                ${$key} = $value;
+            }
+        }
+    }
+
+    // require_once(TEMPLATE_PATH . "/header.php");
+    // require_once(TEMPLATE_PATH . "/menu.php");
+    require_once(VIEW_PATH . "/{$ViewName}.php");
+    // require_once(TEMPLATE_PATH . "/footer.php");
+}
+
 ?>
