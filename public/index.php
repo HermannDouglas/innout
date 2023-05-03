e425001c908d5805b8deb8d377e8e72ae008ca51
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-05-03 10:23:45 -0500

    271. CSS Área de Conteúdo

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-if ($uri === '/' || $uri === '' || '/index.php') {
+if ($uri === '/' || $uri === '' || $uri === '/index.php') {
     $uri = '/login.php';
 }
 
@@ -14,5 +14,14 @@ require_once(CONTROLLER_PATH . "/{$uri}");
 
 
 
+// require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-?>
+// $uri = urldecode(
+//     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+// );
+
+// if ($uri === '/' || $uri === '' || $uri === '/index.php') {
+//     $uri = '/login.php';
+// }
+
+// require_once(CONTROLLER_PATH . "/{$uri}");
