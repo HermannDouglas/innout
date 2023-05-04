1202f28d48a1f7072fbd965b4dd9d5035ca2a915
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-05-03 19:44:34 -0500

    Corrigindo erros no javascript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,7 @@
 (function () {
   const menuToggle = document.querySelector(".menu-toggle");
   menuToggle.onclick = function (e) {
-    const body = docuemnt.querySelector("body");
+    const body = document.querySelector("body");
     body.ClassList.toggle("hide-sidebar");
   };
 })();
