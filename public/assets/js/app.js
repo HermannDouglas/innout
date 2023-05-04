2cbacb9c64fe8aa95c30afa2cefaff9e9c51022a
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-05-04 10:24:51 -0500

    corrigindo função javascript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,6 +2,6 @@
   const menuToggle = document.querySelector(".menu-toggle");
   menuToggle.onclick = function (e) {
     const body = document.querySelector("body");
-    body.ClassList.toggle("hide-sidebar");
+    body.classList.toggle("hide-sidebar");
   };
 })();
