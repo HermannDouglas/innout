73b29b803bac77ff778f243d6fc0d66300fe7bf6
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-05-17 18:22:31 -0500

    307. Inserir Usuário

--- src/models/User.php
+++ src/models/User.php
@@ -1,7 +1,8 @@
 <?php
 // require_once(realpath(MODEL_PATH . '\Model.php'));
 
-class User extends Model {
+class User extends Model
+{
     protected static $tableName = 'users';
     protected static $columns = [
         'id',
@@ -13,7 +14,15 @@ class User extends Model {
         'is_admin',
     ];
 
-    public static function getActiveUsersCount() {
+    public static function getActiveUsersCount()
+    {
         return static::getCount(['raw' => 'end_date IS NULL']);
+    }
+
+    public function insert()
+    {
+        $this->is_admin = $this->is_admin ? 1 : 0;
+        if (!$this->end_date) $this->end_date = null;
+        return parent::insert();
     }
 }
