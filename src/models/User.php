bd6002a5d62ade70469f79c6f5f1e34907f8a2ef
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-05-17 18:28:49 -0500

    308. Ajuste da última aula

--- src/models/User.php
+++ src/models/User.php
@@ -21,8 +21,10 @@ class User extends Model
 
     public function insert()
     {
+        // $this->validate();
         $this->is_admin = $this->is_admin ? 1 : 0;
         if (!$this->end_date) $this->end_date = null;
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         return parent::insert();
     }
 }
